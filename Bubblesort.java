32e29296c652157392857cbc2430bc638da7c479
shakti singh <60788912+shakti1590@users.noreply.github.com>
2022-10-01 23:23:51 +0530

    Update Bubblesort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -2,49 +2,41 @@ import java.util.Arrays;
 
 // Java program for implementation of Bubble Sort
 /*Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order*/
+
 class BubbleSort {
 	void bubbleSort(int arr[])
 	{
-		System.out.println("Original Array: " + Arrays.toString(arr));
-		if(arr==null || arr.length==0)
-			return;
-		boolean is_swapped=false;
 		int n = arr.length;
 		for (int i = 0; i < n - 1; i++)
-		{
-			is_swapped=false;
 			for (int j = 0; j < n - i - 1; j++)
 				if (arr[j] > arr[j + 1]) {
 					// swap arr[j+1] and arr[j]
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
-					is_swapped=true;
 				}
-			if(is_swapped==false)
-				break;
-		}
-		System.out.println("Sorted Array: " + Arrays.toString(arr));
 	}
 
-	// /* Prints the array */
-	// void printArray(int arr[])
-	// {
-	// 	int n = arr.length;
-	// 	for (int i = 0; i < n; ++i)
-	// 		System.out.print(arr[i] + " ");
-	// 	System.out.println();
-	// }
+	/* Prints the array */
+	void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
 
 	// Driver method to test above
 	public static void main(String args[])
 	{
 		BubbleSort ob = new BubbleSort();
-		int arr[] = { 60, 44, 35, 10, 23, 20, 70 };
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
 		ob.bubbleSort(arr);
-		// System.out.println("Sorted array");
-		// ob.printArray(arr);
+		System.out.println("Sorted array");
+		ob.printArray(arr);
 	}
 }
+/* This code is contributed by Shakti */
+
 
 
