6a15fed596662adbe2308b4055e7f1f47179b906
Sanjana Singh <113551964+SanjanaSingh1818@users.noreply.github.com>
2022-10-04 12:22:27 +0530

    Update Bubblesort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -7,6 +7,8 @@ class BubbleSort {
 	void bubbleSort(int arr[]) 
 	{
 		int n = arr.length;
+		
+		//Bubble sort
 		for (int i = 0; i < n - 1; i++)
 			for (int j = 0; j < n - i - 1; j++)
 				if (arr[j] > arr[j + 1]) {
