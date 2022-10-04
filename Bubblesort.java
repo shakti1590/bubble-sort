c57759dec1a77ef4731ee9aa15911f305a177e18
Sonali123676 <83344606+Sonali123676@users.noreply.github.com>
2022-10-05 01:45:54 +0530

    Update Bubblesort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -1,3 +1,4 @@
+//importing java.util.Arrays class
 import java.util.Arrays;
 
 // Java program for implementation of Bubble Sort
@@ -33,7 +34,12 @@ class BubbleSort {
 	{
 		BubbleSort ob = new BubbleSort();
 		int arr[] = { 64, 34, 25, 12, 22, 11, 90 }; //given inputs
+		//printing input array
+		System.out.println("Input array");
+		ob.printArray(arr);
+		//calling bubble sort
 		ob.bubbleSort(arr);
+		//printing sorted array
 		System.out.println("Sorted array");
 		ob.printArray(arr);
 	}
