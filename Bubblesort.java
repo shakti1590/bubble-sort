164eaa3c59bd73f6d0384a3df44a9a413c969a61
Patel1010 <115108605+Patel1010@users.noreply.github.com>
2022-10-06 00:13:40 +0530

    Update Bubblesort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -34,7 +34,7 @@ class BubbleSort {
 	public static void main(String args[])
 	{
 		BubbleSort ob = new BubbleSort();
-		int arr[] = { 64, 34, 25, 12, 22, 11, 90 }; //given inputs
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90 }; 
 		//printing input array
 		System.out.println("Input array");
 		ob.printArray(arr);
