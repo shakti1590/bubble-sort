f44002798cc32725caa437309ba3e71bfd9cfcf2
Piyush Gupta <70501930+piyushspeaks@users.noreply.github.com>
2022-10-05 22:43:55 +0530

    Update Bubblesort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 // Java program for implementation of Bubble Sort
+// Bubble sort is repeatedly swap two adjacent elements if they are in wrong order
 
 
 class BubbleSort {
