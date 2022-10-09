ec07fef438e3b0d6815cf99b8d8b619e2de427ee
THEGAURAVPANDEY <76164354+THEGAURAVPANDEY@users.noreply.github.com>
2022-10-09 10:47:54 +0530

    Update Bubblesort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -8,11 +8,10 @@ import java.util.*;
 class BubbleSort {
 	void bubbleSort(int arr[]) 
 	{
-		int n = arr.length;
 		
 		//Bubble sort
-		for (int i = 0; i < n - 1; i++)
-			for (int j = 0; j < n - i - 1; j++)
+		for (int i = 0; i < arr.length - 1; i++)
+			for (int j = 0; j < arr.length - i - 1; j++)
 				if (arr[j] > arr[j + 1]) {
 					// swap arr[j+1] and arr[j]
 					int temp = arr[j];
@@ -24,8 +23,8 @@ class BubbleSort {
 	
 	void printArray(int arr[])
 	{
-		int n = arr.length;
-		for (int i = 0; i < n; ++i)
+		
+		for (int i = 0; i < arr.length; ++i)
 			System.out.print(arr[i] + " ");
 		System.out.println();
 	}
